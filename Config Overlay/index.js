60ff0d5ab78e44411b07455a8f889f291a5dd8a3
iSMP <javiergcartes14@gmail.com>
2023-11-22 17:55:29 -0300

    First deploy working

---
+++ Config Overlay/index.js
@@ -0,0 +1,37 @@
+let streamerName = document.getElementById("camOverlayText");
+let camText = document.getElementById("cam_text");
+let button = document.getElementById("button");
+let camContainer = document.getElementById("camContainer");
+let styles;
+
+let streamerNameInput = () => {
+  let streamerNameValue = streamerName.value.toString();
+  console.log(streamerNameValue);
+  camText.textContent = streamerNameValue;
+};
+
+streamerName.addEventListener("input", streamerNameInput);
+
+fetch("./src/skinstyle.css")
+  .then((response) => response.text())
+  .then((data) => {
+    styles = `<style>${data}</style>`;
+  });
+
+let newPage = () => {
+  let newHTMLDocument = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      ${styles}
+    </head>
+    <body>
+      ${camContainer.outerHTML}
+    </body>
+    </html>
+  `;
+  let newWindow = window.open();
+  newWindow.document.write(newHTMLDocument);
+};
+
+button.addEventListener("click", newPage);
