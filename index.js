1b4514ac2037a2573af32620d1afcdbea45095dd
iSMP <javiergcartes14@gmail.com>
2023-12-03 18:48:15 -0300

    test webcomponents

--- index.js
+++ index.js
@@ -1,64 +1,70 @@
-let streamerName = document.getElementById("camOverlayText");
-let camText = document.getElementById("cam_text");
-let button = document.getElementById("button");
-let camContainer = document.getElementById("camContainer");
-let styles;
-const HOST_URL = "https://ismp14.github.io/OverlayCustom/";
+class LinkGenerator extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.styles = "";
+    this.hostUrl = "https://ismp14.github.io/OverlayCustom/";
+    this.render();
+    this.attachEvents();
+  }
 
-function getURLParams() {
-  const url = new URL(document.location.href.replace("#", "?"));
-  const params = {};
-  url.searchParams.forEach((value, name) => (params[name] = value));
-  return params;
+  getURLParams() {
+    const url = new URL(document.location.href.replace("#", "?"));
+    const params = {};
+    url.searchParams.forEach((value, name) => (params[name] = value));
+    return params;
+  }
+
+  renderStyles() {
+    fetch("./src/skinstyle.css")
+      .then((response) => response.text())
+      .then((data) => {
+        this.styles = `<style>${data}</style>`;
+      });
+  }
+
+  render() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+      <style>
+        /* Estilos para el componente */
+      </style>
+      <div>
+        <input id="camOverlayText" type="text">
+        <button id="button">Generar Enlace</button>
+        <input class="overlayUrl" type="text" readonly>
+      </div>
+    `;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.renderStyles();
+  }
+
+  attachEvents() {
+    const button = this.shadowRoot.getElementById("button");
+    const camOverlayText = this.shadowRoot.getElementById("camOverlayText");
+    const overlayUrl = this.shadowRoot.querySelector(".overlayUrl");
+
+    button.addEventListener("click", () => {
+      const camTextContent = camOverlayText.value.toString();
+      const url = new URL("./overlay/index.html", this.hostUrl);
+      url.searchParams.set("Overlay", camTextContent);
+
+      overlayUrl.value = url.toString();
+      localStorage.setItem("newURL", url.toString());
+    });
+
+    window.onload = () => {
+      const params = this.getURLParams();
+      if (params["Overlay"]) {
+        camOverlayText.value = params["Overlay"];
+      }
+    };
+
+    camOverlayText.addEventListener("input", () => {
+      const streamerNameValue = camOverlayText.value.toString();
+      localStorage.setItem("camText", streamerNameValue);
+    });
+  }
 }
 
-let streamerNameInput = () => {
-  let streamerNameValue = streamerName.value.toString();
-  console.log(streamerNameValue);
-  camText.textContent = streamerNameValue;
-  localStorage.setItem("camText", streamerNameValue);
-};
-
-streamerName.addEventListener("input", streamerNameInput);
-
-fetch("./src/skinstyle.css")
-  .then((response) => response.text())
-  .then((data) => {
-    styles = `<style>${data}</style>`;
-  });
-
-window.onload = function () {
-  let params = getURLParams();
-  if (params["Overlay"]) {
-    let cam_text = document.getElementById("cam_text");
-    cam_text.textContent = params["Overlay"];
-  }
-};
-
-let newPage = () => {
-  let camTextContent = localStorage.getItem("camText");
-
-  let newHTMLDocument = `
-            <!DOCTYPE html>
-            <html>
-            <head>
-              ${styles}
-            </head>
-            <body>
-              ${camContainer.outerHTML}
-            </body>
-            </html>
-          `;
-
-  localStorage.setItem("newHTMLDocument", newHTMLDocument);
-
-  let url = new URL("./overlay/index.html", HOST_URL);
-  url.searchParams.set("Overlay", camTextContent);
-
-  localStorage.setItem("newURL", url.toString());
-
-  let overlayUrl = document.querySelector(".overlayUrl");
-  overlayUrl.value = url.toString();
-};
-
-button.addEventListener("click", newPage);
+customElements.define("link-generator", LinkGenerator);
